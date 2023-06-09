00cd016d13b3c8afb6825c3b9799fc14bd41f992
juanimelia <juanimeliausa@gmail.com>
2023-06-09 17:27:13 -0300

    first commit

---
+++ js/apiGet.js
@@ -0,0 +1,37 @@
+
+/* GET DE API */
+
+async function fetchApi() {
+    try {
+      const response = await fetch('https://sea-feliz.up.railway.app/api/productos/');
+      const data = await response.json();
+  
+      const listaDeProductos = document.getElementById('listaDeProductos');
+  
+      data.productos.forEach(producto => {
+        
+        const productoItem = document.createElement('div');
+        productoItem.classList.add("producto");
+  
+        // Utilizar un template HTML
+        productoItem.innerHTML = `
+          <img  src="${producto.img}" alt="${producto.nombre}">
+          <h3>${producto.nombre}</h3>
+          <p>Precio: $${producto.precio}</p>
+          <p>Categoria: ${producto.categoria}</p>
+          <p>Stock: ${producto.stock}</p>
+        `;
+        
+        listaDeProductos.appendChild(productoItem);
+      });
+    
+  
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  fetchApi();
+  
+  
