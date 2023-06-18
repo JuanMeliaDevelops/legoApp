a59a9f9598dc72af54013d32c9dabe8617d3cd26
juanimelia <juanimeliausa@gmail.com>
2023-06-18 03:37:49 -0300

    Funcionamiento Controller insertProductos.js

--- js/controller/insertProductos.js
+++ js/controller/insertProductos.js
@@ -5,39 +5,46 @@ async function uploadProduct() {
 
     let nombreProducto = document.getElementById("nombreProducto").value;
     let precio = document.getElementById("precio").value;
+    let descripcion = document.getElementById("descripcion").value;
+    let dimensiones = document.getElementById("dimensiones").value;
     let categoria = document.getElementById("categoria").value;
     let stock = document.getElementById("stock").value;
-
     let img = document.getElementById("img").files[0];
-
-
-    const producto = {
-        nombreProducto,
-        precio,
-        categoria,
-        stock,
-        img
-    };
 
 
     const formData = new FormData();
     formData.append("img", img);
-    formData.append("producto", JSON.stringify(producto));
+    formData.append("nombre", nombreProducto);
+    formData.append("categoria", categoria);
+    formData.append("stock", parseInt(stock));
+    formData.append("dimensiones", dimensiones);
+    formData.append("precio", precio);
+    formData.append("descripcion", descripcion);
+
+
+    console.log(formData);
 
 
     const requestOptions = {
         method: "POST",
         body: formData,
         headers: {
-          "x-token": token,
+            "x-token": token,
         },
-      };
+    };
     try {
 
-        const response = await fetch('https://sea-feliz.up.railway.app/productos/', requestOptions);
+        const response = await fetch('https://sea-feliz.up.railway.app/api/productos', requestOptions);
         const data = await response.json();
 
-        console.log("El producto se ha insertado correctamente.  ", data)
+        console.log("Se esta intentando subir el producto.  ", data)
+
+        if (data.msg == "Producto guardado") {
+            document.getElementById("mensajeUpload").innerText = "El producto se ha registrado correctamente!";
+            setTimeout(() => {
+                window.location.href = "adminPanel.html";
+            }, 3000);
+        }
 
 
 
